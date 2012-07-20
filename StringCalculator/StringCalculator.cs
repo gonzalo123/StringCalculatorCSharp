bd40182115b4a7e34d1628113b5b5e4ec61432d9
Gonzalo Ayuso <gonzalo.ayuso@perfiles.arcelor.com>
2012-07-20 12:10:56 +0200

    Initial Commit

---
+++ StringCalculator/StringCalculator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringCalculator
+{
+    public class StringCalculator
+    {
+        private string separator = ",";
+        private string myString;
+        private const int maxNumber = 1000;
+        private List<string> separators = new List<string>() {"\n"};
+
+        public int Add(string numbers)
+        {
+            if (numbers == "") return 0;
+            myString = numbers;
+            if (hasCustomSeparator())
+            {
+                appendToSeparators(hasMultibyteSeparator());
+                myString = getNewString();
+            }
+
+            unifySeparators();
+            if (myString.IndexOf(separator) > 0)
+                return processStingWithSeparator();
+            else
+                return Convert.ToInt32(myString);
+        }
+
+        private int processStingWithSeparator()
+        {
+            string[] pieces = splitInput(myString, separator);
+            int output = 0;
+            foreach (string piece in pieces)
+            {
+                int value = Convert.ToInt32(piece);
+                if (value < 0) throw new ArgumentException("negatives not allowed");
+                if (value < maxNumber) output += value;
+            }
+            return output;
+        }
+
+        private void unifySeparators()
+        {
+            foreach (string _separator in separators) 
+                myString = myString.Replace(_separator, separator);
+        }
+
+        private static string[] splitInput(string numbers, string separator)
+        {
+            return numbers.Split(new string[] { separator }, StringSplitOptions.None);
+        }
+
+        private string getNewString()
+        {
+            int start = myString.IndexOf("\n") + 1;
+            int length = myString.Length - start;
+            return myString.Substring(start, length);
+        }
+
+        private void appendToSeparators(bool withMultibyteSeparator = false)
+        {
+            int start = withMultibyteSeparator ? 3 : 2;
+            string endString = withMultibyteSeparator ? "]\n" : "\n";
+            string mySeparator = myString.Substring(start, myString.IndexOf(endString) - start);
+
+            if (hasMultipleSeparators(mySeparator))
+            {
+                foreach (string multipleSeparatorElement in getMultipleSeparators(mySeparator))
+                {
+                    separators.Add(multipleSeparatorElement);
+                }
+            }
+            else
+            {
+                separators.Add(mySeparator);
+            }
+        }
+
+        private string[] getMultipleSeparators(string mySeparator)
+        {
+            return mySeparator.Split(new string[] { "][" }, StringSplitOptions.None);
+        }
+
+        private bool hasMultipleSeparators(string mySeparator)
+        {
+            return mySeparator.IndexOf("][") >= 0;
+        }
+
+        private bool hasCustomSeparator()
+        {
+            return myString.IndexOf("//") >= 0;
+        }
+
+        private bool hasMultibyteSeparator()
+        {
+            return myString.IndexOf("//[") >= 0;
+        }
+    }
+}
---
+++ TestStringCalculator/StringCaculatorTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StringCalculator;
+
+namespace TestStringCalculator
+{
+    [TestClass]
+    public class StringCaculatorTest
+    {
+        [TestMethod]
+        public void TestSimpleNumbers()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            Assert.AreEqual(0, stringCalculator.Add(""));
+            Assert.AreEqual(1, stringCalculator.Add("1"));
+            Assert.AreEqual(2, stringCalculator.Add("2"));
+            Assert.AreEqual(21, stringCalculator.Add("21"));
+        }
+
+        [TestMethod]
+        public void TwoNumbers()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            Assert.AreEqual(2, stringCalculator.Add("1,1"));
+            Assert.AreEqual(3, stringCalculator.Add("1,2"));
+            Assert.AreEqual(33, stringCalculator.Add("11,22"));
+        }
+
+        [TestMethod]
+        public void MoreThan2()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            Assert.AreEqual(3, stringCalculator.Add("1,1,1"));
+            Assert.AreEqual(6, stringCalculator.Add("1,2,3"));
+        }
+
+        [TestMethod]
+        public void MoreThan1Separator()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            Assert.AreEqual(3, stringCalculator.Add("1\n1,1"));
+            Assert.AreEqual(6, stringCalculator.Add("1\n2,3"));
+        }
+
+        [TestMethod]
+        public void CustomDemiters()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            Assert.AreEqual(3, stringCalculator.Add("//;\n1;2"));
+            Assert.AreEqual(15, stringCalculator.Add("//;\n12;3"));
+            Assert.AreEqual(16, stringCalculator.Add("//+\n12+3+1"));
+        }
+
+        [TestMethod]
+        public void CustomDemitersVariousCharacters()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            Assert.AreEqual(3, stringCalculator.Add("//[**]\n1**2")); 
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))] 
+        public void TestNegativeNumbers()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            try
+            {
+                stringCalculator.Add("-1,2");
+            }
+            catch (ArgumentException exception)
+            {
+                Assert.AreEqual("negatives not allowed", exception.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        public void IgnoredNumbersHigherThan1000()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            Assert.AreEqual(2, stringCalculator.Add("2,1000")); 
+        }
+
+        [TestMethod]
+        public void MultipleDelimiters()
+        {
+            var stringCalculator = new StringCalculator.StringCalculator();
+            Assert.AreEqual(10, stringCalculator.Add("//[**][$$]\n1**2$$3,4"));
+        } 
+    }
+}
